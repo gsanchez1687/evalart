fd9536f9cac036a3a50afd61fba2cf726fc7950b
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-07-16 18:07:37 -0500

    agregando mas pruebas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -105,5 +105,101 @@ function encontrarCaminoMinimo($array) {
 $myArray = array(1, 2, 9, 2, 5, 3, 5, 1, 5);
 encontrarCaminoMinimo($myArray);
 
-?>
 
+function checkQuestionMark($string) {
+    $longitud = strlen($string);
+    $num = [];
+    for ($i = 0; $i < $longitud; $i++) {
+        if (is_numeric($string[$i])) {
+            $actual = intval($string[$i]);
+            if (!empty($num)) {
+                foreach ($num as $preview) {
+                    if ($actual + $preview['numero'] == 10) {
+                        $signosInterrogacion = substr_count($string, '?', $preview['posicion'] + 1, $i - $preview['posicion'] - 1);
+                        if ($signosInterrogacion != 3) {
+                            return "false";
+                        }
+                    }
+                }
+            }
+            $num[] = ['numero' => $actual, 'posicion' => $i];
+        }
+    }
+    
+    foreach ($num as $preview) {
+        foreach ($num as $actual) {
+            if ($actual['posicion'] > $preview['posicion'] && $actual['numero'] + $preview['numero'] == 10) {
+                return "true";
+            }
+        }
+    }
+    
+    return "false";
+}
+
+echo checkQuestionMark("aa6?9");
+echo "\n";
+echo checkQuestionMark("acc?7??sss?3rr1??????5");
+echo "\n";
+echo checkQuestionMark("arrb6???4xxbl5???eee5");
+
+
+function sumMinMaxMatriz($matriz) {
+    $min = $matriz[0][0];
+    $max = $matriz[0][0];
+    foreach ($matriz as $fila) {
+        foreach ($fila as $numero) {
+            if ($numero < $min) {
+                $min = $numero;
+            }
+            if ($numero > $max) {
+                $max = $numero;
+            }
+        }
+    }
+    $suma = $min + $max;
+    echo "La suma del número mínimo y máximo es: " . $suma;
+}
+
+// Ejemplo de uso
+$matriz = [
+    [2, 5, 4],
+    [6, 2, 11],
+    [10, 22, 45]
+];
+echo "\n";
+sumMinMaxMatriz($matriz);
+
+
+
+function calcularComision($ventas, $meta) {
+    $cumplimiento = ($ventas / $meta) * 100;
+    $Comision = 0;
+
+    if ($cumplimiento <= 100) {
+        $Comision = $cumplimiento;
+    } else {
+        if ($cumplimiento > 120 && $cumplimiento <= 125) {
+            $Comision = 102;
+        } elseif ($cumplimiento > 125 && $cumplimiento <= 130) {
+            $Comision = 103;
+        } elseif ($cumplimiento > 130 && $cumplimiento <= 149) {
+            $Comision = 104;
+        } elseif ($cumplimiento > 149) {
+            $Comision = 105;
+        }
+    }
+
+    $comisionFinal = ($Comision / 100) * $ventas;
+    return $comisionFinal;
+}
+echo calcularComision(100, 80);
+
+
+//SELECT e.nombre, COUNT(p.id) AS partidos_visitante
+//FROM equipos.equipos e
+//JOIN equipos.partidos p ON e.id = p.equipo_visitante
+//GROUP BY e.nombre
+//HAVING COUNT(p.id) > 5;
+
+?>
